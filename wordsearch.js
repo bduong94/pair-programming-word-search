b89adfa3baa70eff2725f777d94532d5150de3e1
Brian Duong <brianminhduong@gmail.com>
2021-11-19 02:39:56 +0000

    Remove diagonalTranpose

--- wordsearch.js
+++ wordsearch.js
@@ -9,7 +9,7 @@ const transpose = function(matrix) {
     }
     return resultArray;
   };
-
+  
 const printMatrix = (matrix) => {
     for (const row of matrix) {
         for (const el of row) {
@@ -19,41 +19,41 @@ const printMatrix = (matrix) => {
     }
 };
 
-//transposeDiagonal
-/*
-[1 2 3] 
-[4 5 6]
-[7 8 9] 
+  //Loop for number rows
+  //Once number of rows reached
+  //Loop for number of columns -- i = row.length
 
-[9 6 3]
-[8 5 2]
-[7 4 1]
+  const coordinatesValid = function (matrix, i, j) {
+    let noRows = matrix.length;
+    let noCols = matrix[0].length;
+    return (i >= 0 && i < noRows) && (j >= 0 && j < noCols);
+  }
 
-[0,0] [0,1] [0,2]
-[1,0] [1,1] [1,2]
-[2,0] [2,1] [2,2]
+  const generateStringsDiagonally = (matrix) => {
+    let arrayOfStrings = [];
+    for (let i = 0; i < matrix.length; i++) {
+        let thisRow = []
+        for (let j = 0; j < matrix[0].length; j++)  {
+            if (j > i) break;
+            thisRow.push(matrix[i-j][j]);
+        }
+        arrayOfStrings.push(thisRow);
+    }   
+    for (let i = 1; i < matrix[0].length; i++) {
+      let thisRow = [];
+      let currentCol, currentRow; 
+      for (let j = 0; j < matrix.length; j++) {
+        currentRow = matrix.length - 1 - j;
+        currentCol = i + j;
+        if (coordinatesValid(matrix, currentRow, currentCol)){
+          thisRow.push(matrix[currentRow][currentCol]);
+        }
+      }
+      arrayOfStrings.push(thisRow);
+    }
 
-[2,2] [1,2] [0, 2]
-[2,1] [1,1] [0, 1]
-[2,0] [1,0] [0, 0]
-
-[i,j-0] [i-1,j-0] [i-2, j-0] [p = 0, q =0] [p = 0, q = 1] [p = 0, q = 2]
-[i,j-1] [i-1,j-1] [i-2, j-1] [p = 1, q =0] [p = 1, q = 1] [p = 1, q = 2]
-[i,j-2] [i-1,j-2] [i-2, j-2] [p = 2, q =0] [p = 2, q = 1] [p = 2, q = 2]
-*/
-
-const letters = [
-    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-    ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-    ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-    ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-  ];
-
+    return arrayOfStrings;
+}
 
 const makeArray = (rows, cols) => {
     let array = [];
@@ -67,24 +67,24 @@ const makeArray = (rows, cols) => {
     return array;
 }
   
-const transposeDiagonally = (matrix) => {
-    if (!matrix.length) return [];
-    let rowsOfMatrix = matrix.length;
-    let colsOfMatrix = matrix[0].length;
-    let newMatrix = makeArray(rowsOfMatrix, colsOfMatrix);
+// const transposeDiagonally = (matrix) => {
+//     if (!matrix.length) return [];
+//     let rowsOfMatrix = matrix.length;
+//     let colsOfMatrix = matrix[0].length;
+//     let newMatrix = makeArray(rowsOfMatrix, colsOfMatrix);
   
-    for (let p = 0; p < rowsOfMatrix; p++) {
-      for (let q = 0; q < colsOfMatrix; q++){
-        newMatrix[p][q] = matrix[rowsOfMatrix - 1 - q][colsOfMatrix - 1 - p];
-      }
-    }
+//     for (let p = 0; p < rowsOfMatrix; p++) {
+//       for (let q = 0; q < colsOfMatrix; q++){
+//         newMatrix[p][q] = matrix[rowsOfMatrix - 1 - q][colsOfMatrix - 1 - p];
+//       }
+//     }
   
-    return newMatrix;
-  }
+//     return newMatrix;
+//   }
   
 //   printMatrix(makeArray(3,3));
 //   printMatrix(transposeDiagonally(letters));
-  printMatrix(transposeDiagonally([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
+  // printMatrix(transposeDiagonally([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
   
 //   //Generate List of stings:
 //   // A
@@ -92,19 +92,6 @@ const transposeDiagonally = (matrix) => {
 //   // Y E C
 
 //
-const generateStringsDiagonally = (matrix) => {
-    let arrayOfStrings = [];
-    for (let i = 0; i < matrix.length; i++) {
-        let thisRow = []
-        for (let j = 0; j < matrix[0].length; j++)  {
-            if (j > i) break;
-            thisRow.push(matrix[i-j][j]);
-        }
-        arrayOfStrings.push(thisRow);
-    } 
-    return arrayOfStrings;
-}
-
 console.log(generateStringsDiagonally([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
 
 //         [0][0]
@@ -119,14 +106,15 @@ const checkRowsForWord = (letters, word) => {
 }
 
 const wordSearchBottomUpTopRight = (letters, word) => {
-    const foundInFirstHalf = checkRowsForWord(generateStringsDiagonally(letters), word);
-    const foundInSecondHalf = checkRowsForWord(generateStringsDiagonally(transposeDiagonally(letters)), word);
-    return foundInFirstHalf || foundInSecondHalf ? true : false;    
+    return checkRowsForWord(generateStringsDiagonally(letters), word);
+    // const foundInSecondHalf = checkRowsForWord(generateStringsDiagonally(transposeDiagonally(letters)), word);
+    //return foundInFirstHalf || foundInSecondHalf ? true : false;    
 }
 
 
 const wordSearch = (letters, word) => { 
     if(typeof word !== 'string' || word.length === 0) return false;
+    if (!(Array.isArray(letters) && letters.length)) return false;
     //Horizontally/Vertically
     const foundWordHorizontally = checkRowsForWord(letters, word);
     const foundWordVertically = checkRowsForWord(transpose(letters), word);
@@ -134,11 +122,19 @@ const wordSearch = (letters, word) => {
     const foundWordHorizontallyReverse = checkRowsForWord(letters, reverseWord);
     const foundWordVerticallyReverse = checkRowsForWord(transpose(letters), reverseWord);
     //Diagonally
-    const foundWordDiagonally = wordSearchBottomUpTopRight(letters,word);
-
-
-
-    return foundWordHorizontally || foundWordVertically || foundWordHorizontallyReverse || foundWordVerticallyReverse || foundWordDiagonally ? true : false;
+    const foundWordDiagonallyBtmLeftTopRight = wordSearchBottomUpTopRight(letters,word);
+    const foundWordDiagonallyBtmLeftTopRightReverse = wordSearchBottomUpTopRight(letters,reverseWord);
+    const foundWordDiagonallyTopLeftBtmRight = wordSearchBottomUpTopRight(transpose(letters),word);
+    const foundWordDiagonallyTopLeftBtmRightReverse = wordSearchBottomUpTopRight(transpose(letters),reverseWord);
+    return foundWordHorizontally 
+      || foundWordVertically 
+      || foundWordHorizontallyReverse
+      || foundWordVerticallyReverse
+      || foundWordDiagonallyBtmLeftTopRight
+      || foundWordDiagonallyBtmLeftTopRightReverse
+      || foundWordDiagonallyTopLeftBtmRight
+      || foundWordDiagonallyTopLeftBtmRightReverse
+      ? true : false;
 }
 
 module.exports = wordSearch
